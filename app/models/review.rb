f67ce7d539f8372f911c90906b1b5e212b2d0205
Vadim19 <the.tarasov@gmail.com>
2018-03-14 18:04:49 +0200

    Added counter_cache to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :restaurant
+  belongs_to :restaurant, counter_cache: true
   validates :restaurant_id,
             :reviewer_name,
             :rating,
